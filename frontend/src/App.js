bf3eea570cd6b573f646830fc30b9d1ea7bddbb7
scottkonner <scott_konner@yahoo.com>
2022-12-22 19:46:14 -0500

    react Authenticate Me 3 Navigation Complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,20 +14,25 @@ function App() {
   }, [dispatch]);
 
   return isLoaded && (
-    <Switch>
-      <Route path="/" exact>
-        This is a home page
-      </Route>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-      <Route path="/signup">
-        <SignupFormPage />
-      </Route>
-      <Route path="/" >
-        This page means you spelled your route incorrectly
-      </Route>
-    </Switch>
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/" exact>
+            This is a home page
+          </Route>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+          <Route path="/" >
+            This page means you spelled your route incorrectly
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 }
 export default App;
